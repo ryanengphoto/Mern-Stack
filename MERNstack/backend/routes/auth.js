80796f0492d1d53f55972bfdbbb2a5c9d02ba286
Ryan Eng <rye2020@yahoo.com>
2025-10-14 16:09:47 -0400

    comment

--- MERNstack/backend/routes/auth.js
+++ MERNstack/backend/routes/auth.js
@@ -21,7 +21,7 @@ router.post('/register', async (req, res) => {
   }
 });
 
-// LOGIN
+// LOGIN, generate JWT Token for frontend to hold for privileged requests
 router.post('/login', async (req, res) => {
   try {
     const { email, password } = req.body;
